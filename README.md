5164856ad429febacc068e84508a6c65f3992364
Terunami <nobooknolife16@yahoo.co.jp>
2020-06-26 00:19:40 +0900

    gmail_credential

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Send-Gmail-by-Python
 Pythonを使ったGmailの自動送信を実装してみる。
+
+参考先： https://qiita.com/muuuuuwa/items/822c6cffedb9b3c27e21
---
+++ gmail_credential.py
@@ -0,0 +1,35 @@
+import ps
+import pickle
+from google.auth.transport.requests import requests
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# Gmail APIのスコープを設定
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.modify",
+]
+
+
+def get_credential():
+    """
+    アクセストークンの取得
+
+    カレントディレクトリに pickle 形式でトークンを保存し、再利用できるようにする。
+    """
+    creds = None
+    if ps.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("client_id.json", SCOPES)
+            # creds = flow.run_local_server()
+            creds = flow.run_console()
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+    return creds
+    
